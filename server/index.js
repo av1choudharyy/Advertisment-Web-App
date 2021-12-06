f9a2958bd23ee80ae4b3100a98f94c1c2c299fac
Abhishek <choudharyabhishek2199@gmail.com>
2021-12-06 16:56:08 +0530

    Deployment Necessities

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config()
 
 app.use(express.json());
 app.use(cors());
@@ -8,15 +9,18 @@ app.use(cors());
 const db = require("./models");
 
 // Routers
-const postRouter = require("./routes/Posts");
+const postRouter = require("./routes/posts");
 app.use("/posts", postRouter);
-const commentsRouter = require("./routes/Comments");
+const commentsRouter = require("./routes/comments");
 app.use("/comments", commentsRouter);
-const usersRouter = require("./routes/Users");
+const usersRouter = require("./routes/users");
 app.use("/auth", usersRouter);
 
 db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
+  app.listen( process.env.PORT || 3001, () => {
     console.log("Server running on port 3001");
   });
-});
+})
+.catch((err)=>{
+  console.log(err)
+})
